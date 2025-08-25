06a8eb42276f1920f729ddd7883a3e6bef7e07f2
ThomasKrieger <thomas.krieger@vmlens.com>
2025-08-25 11:37:42 +0200

    uodate vmlens version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
-    testImplementation("com.vmlens:api:1.2.11")
+    testImplementation("com.vmlens:api:1.2.12")
     testImplementation("org.hamcrest:java-hamcrest:2.0.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
@@ -22,15 +23,16 @@ dependencies {
 buildscript {
     repositories {
         mavenCentral()
+        mavenLocal()
     }
     dependencies {
-        classpath("com.vmlens:standalone:1.2.11")
+        classpath("com.vmlens:standalone:1.2.12")
     }
 }
 
 tasks.register("vmlensReport") {
     doLast {
-        VMLens().process(layout.buildDirectory.getAsFile().get());
+        VMLens().withShowAllRuns().process(layout.buildDirectory.getAsFile().get());
     }
 }
 
